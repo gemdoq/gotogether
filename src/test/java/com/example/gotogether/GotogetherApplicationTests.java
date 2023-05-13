500f084a60b513907d493711412739b46902aeb8
SeungHo <gemdoq@gmail.com>
2023-05-13 18:27:04 +0900

    first commit: initial setup for project

---
+++ src/test/java/com/example/gotogether/GotogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
