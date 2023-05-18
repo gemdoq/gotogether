9e63b59011cc4a73984dbce961d01a0df5c37b6e
SeungHo <gemdoq@gmail.com>
2023-05-18 20:48:53 +0900

    feature(UserEntity): add toEntity()

--- src/main/java/com/example/gotogether/domain/user/entity/UserEntity.java
+++ src/main/java/com/example/gotogether/domain/user/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package com.example.gotogether.domain.user.entity;
 
+import com.example.gotogether.domain.user.dto.UserCreateRequest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,5 +28,17 @@ public class UserEntity {
     private String emailAddress;
     private String phoneNumber;
 
+    public UserEntity(String username, String account, String password, String emailAddress, String phoneNumber) {
+    }
+
+    public static UserEntity toEntity(UserCreateRequest req) {
+        return new UserEntity(
+                req.getUsername(),
+                req.getAccount(),
+                req.getPassword(),
+                req.getEmailAddress(),
+                req.getPhoneNumber()
+        );
+    }
 }
 
