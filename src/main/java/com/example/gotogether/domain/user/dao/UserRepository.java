37e687f795daf2ff2895e7c1a54c664959a47180
SeungHo <gemdoq@gmail.com>
2023-05-18 21:02:48 +0900

    feature(UserRepository): add findByEmailAddress()

--- src/main/java/com/example/gotogether/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/gotogether/domain/user/dao/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.gotogether.domain.user.dao;
 import com.example.gotogether.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmailAddress(String emailAddress);
 }
