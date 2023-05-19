3eb05601dde441080aafa2d49bf5b71294a2ed37
SeungHo <gemdoq@gmail.com>
2023-05-19 20:21:19 +0900

    feature(AppException): create customize AppException extends RuntimeException

--- src/main/java/com/example/gotogether/global/error/AppException.java
+++ src/main/java/com/example/gotogether/global/error/AppException.java
@@ -1,13 +1,18 @@
 package com.example.gotogether.global.error;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AppException extends RuntimeException {
 
     private ErrorCode errorCode;
-    private String message;
+    private String appExceptionMessage;
 
     public AppException(ErrorCode errorCode) {
         this.errorCode = errorCode;
-        this.message = errorCode.getErrorMessage();
+        this.appExceptionMessage = errorCode.getErrorCodeMessage();
     }
 
 }
