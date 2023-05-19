cbc54d46feee65c56e3facc18d16f7cfefeec635
SeungHo <gemdoq@gmail.com>
2023-05-19 20:51:09 +0900

    refactor(UserRestController): refactor return type UUID(String) to ResponseEntity<Response<UserCreateResponse>>

--- src/main/java/com/example/gotogether/domain/user/api/UserRestController.java
+++ src/main/java/com/example/gotogether/domain/user/api/UserRestController.java
@@ -2,11 +2,12 @@ package com.example.gotogether.domain.user.api;
 
 import com.example.gotogether.domain.user.application.UserService;
 import com.example.gotogether.domain.user.dto.UserCreateRequest;
+import com.example.gotogether.domain.user.dto.UserCreateResponse;
+import com.example.gotogether.global.response.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -17,11 +18,12 @@ public class UserRestController {
     private final UserService userService;
 
     @PostMapping("")
-    public String userCreate(@RequestBody UserCreateRequest req) {
+    public ResponseEntity<Response<UserCreateResponse>> userCreate(@RequestBody UserCreateRequest req) {
+        log.info("유저 생성 요청 컨트롤러 시작됩니다.");
         log.info("유저 생성 요청이 들어왔습니다.");
-        UUID userId = userService.createUser(req);
+        UserCreateResponse res = userService.createUser(req);
         log.info("유저 생성 요청이 처리되었습니다.");
-        log.info("생성된 유저 id : {}", userId);
-        return "유저 생성 성공, UUID : " + userId;
+        log.info("유저 생성 요청 컨트롤러 종료됩니다.");
+        return ResponseEntity.ok(Response.success(res));
     }
 }
