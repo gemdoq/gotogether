aec42cc0f05b16ebfab3745cf30e3d2e23a0ab49
SeungHo <gemdoq@gmail.com>
2023-05-19 22:19:12 +0900

    feature(UserRestController): add userRead()

--- src/main/java/com/example/gotogether/domain/user/api/UserRestController.java
+++ src/main/java/com/example/gotogether/domain/user/api/UserRestController.java
@@ -3,6 +3,7 @@ package com.example.gotogether.domain.user.api;
 import com.example.gotogether.domain.user.application.UserService;
 import com.example.gotogether.domain.user.dto.UserCreateRequest;
 import com.example.gotogether.domain.user.dto.UserCreateResponse;
+import com.example.gotogether.domain.user.dto.UserReadResponse;
 import com.example.gotogether.global.response.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +20,21 @@ public class UserRestController {
 
     @PostMapping("")
     public ResponseEntity<Response<UserCreateResponse>> userCreate(@RequestBody UserCreateRequest req) {
-        log.info("유저 생성 요청 컨트롤러 시작됩니다.");
-        log.info("유저 생성 요청이 들어왔습니다.");
+        log.info("유저 생성 요청 컨트롤러 시작합니다.");
+        log.info("유저 생성 요청 들어왔습니다.");
         UserCreateResponse res = userService.createUser(req);
-        log.info("유저 생성 요청이 처리되었습니다.");
-        log.info("유저 생성 요청 컨트롤러 종료됩니다.");
+        log.info("유저 생성 요청 처리했습니다.");
+        log.info("유저 생성 요청 컨트롤러 종료합니다.");
+        return ResponseEntity.ok(Response.success(res));
+    }
+
+    @GetMapping("/{account}")
+    public ResponseEntity<Response<UserReadResponse>> userRead(@PathVariable String account) {
+        log.info("유저 조회 요청 컨트롤러 시작합니다.");
+        log.info("유저 조회 요청 들어왔습니다.");
+        UserReadResponse res = userService.readUserByAccount(account);
+        log.info("유저 조회 요청 처리했습니다.");
+        log.info("유저 조회 요청 컨트롤러 종료합니다.");
         return ResponseEntity.ok(Response.success(res));
     }
 }
