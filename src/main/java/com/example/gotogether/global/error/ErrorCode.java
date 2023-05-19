1c7cc578cfdf066aaaca6c3f79f764c332a563c5
SeungHo <gemdoq@gmail.com>
2023-05-19 22:20:59 +0900

    feature(ErrorCode): add USER_NOT_FOUND

--- src/main/java/com/example/gotogether/global/error/ErrorCode.java
+++ src/main/java/com/example/gotogether/global/error/ErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 기존 DB와 중복
-    DUPLICATED_USER(HttpStatus.CONFLICT, "이미 등록된 유저입니다.");
+    DUPLICATED_USER(HttpStatus.CONFLICT, "이미 등록된 유저입니다."),
+
+    // DB 리소스 없음
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보가 없습니다.");
 
     private HttpStatus httpStatus;
     private String errorCodeMessage;
